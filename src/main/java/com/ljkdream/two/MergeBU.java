321527047b8d0ad2c835110bae211d948a7f5169
L.J.K <1013939150@qq.com>
2015-12-02 13:24:32 +0800

    备份笔记

--- src/main/java/com/ljkdream/two/MergeBU.java
+++ src/main/java/com/ljkdream/two/MergeBU.java
@@ -13,11 +13,11 @@ public class MergeBU extends AbstractMerge {
 
     @Override
     public void sort(Comparable[] a) {
-        int N = a.length;
+        int N = a.length; //进行 lgN 次两两归并
         this.initAus(N);
 
-        for (int sz = 1; sz < N; sz *= 2) {
-            for (int lo = 0; lo < N - sz; lo += 2 * sz) {
+        for (int sz = 1; sz < N; sz *= 2) { //sz 子数组大小
+            for (int lo = 0; lo < N - sz; lo += 2 * sz) { //lo：子数组索引
                 merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
             }
         }
