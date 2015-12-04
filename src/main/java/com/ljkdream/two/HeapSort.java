d2d141e5f15d27872a10d2f8cd25ed3ba2fe9b53
L.J.K <1013939150@qq.com>
2015-12-04 13:42:53 +0800

    修改 堆排序代码

--- src/main/java/com/ljkdream/two/HeapSort.java
+++ src/main/java/com/ljkdream/two/HeapSort.java
@@ -1,5 +1,8 @@
 package com.ljkdream.two;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * 堆排序
  * Created by LJK on 2015-12-04.
@@ -7,26 +10,51 @@ package com.ljkdream.two;
 public class HeapSort extends AbstractSortExample {
     @Override
     public void sort(Comparable[] a) {
-        int N = a.length;
-        for (int k = N/2; k >= 1; k--) {
-            sink(a, k, N);
+        Comparable[] c = new Comparable[a.length + 1];
+        System.arraycopy(a, 0, c, 1, a.length);
+
+
+        int N = c.length; //0~11
+        for (int k = N / 2; k >= 1; k--) { //从最右侧的子堆根节点开始建堆
+            sink(c, k, N);
         }
 
-//        sink(a, 1, N);
+        while (N > 1) {
+            exch(c, 1, --N);
+            sink(c, 1, N);
+        }
 
+        //还原回去
+        for (int i = 0; i < a.length; i++) {
+            a[i] = c[i + 1];
+        }
     }
 
     private void sink(Comparable[] a, int k, int n) {
-        while (k * 2 <= n) {
-            int j = k * 2;
-            if (j < n-1 && less(j, j + 1)) { //获取子孩子值大的那个元素的下标
-                j++;
+        while (leftChile(k) < n) { //获取子孩子值大的那个元素的下标
+            int maxIdx = leftChile(k);
+            int rightIdx = rightChild(k);
+
+            if (rightIdx < n && less(a[maxIdx], a[rightIdx])) {
+                maxIdx = rightIdx;
             }
 
-            if (less(k, j)) {
-                exch(a, k, j);
+            if (less(a[k], a[maxIdx])) {
+                exch(a, k, maxIdx);
             }
-            k = j;
+            k = maxIdx;
         }
+    }
+
+    public int parent(int k) {
+        return k / 2;
+    }
+
+    public int leftChile(int p) {
+        return 2 * p;
+    }
+
+    public int rightChild(int p) {
+        return 2 * p + 1;
     }
 }
