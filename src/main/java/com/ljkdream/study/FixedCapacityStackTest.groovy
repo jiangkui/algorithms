779589420a7cf36cbcdce662601007c79b06b050
jiangkui <1013939150@qq.com>
2016-01-22 09:33:58 +0800

    新增 groovy 测试

--- src/main/java/com/ljkdream/study/FixedCapacityStackTest.groovy
+++ src/main/java/com/ljkdream/study/FixedCapacityStackTest.groovy
@@ -1,27 +1,27 @@
-package com.ljkdream.study
-
-/**
- * 测试代码
- * Created by ljk on 16-1-22.
- */
-class FixedCapacityStackTest extends GroovyTestCase {
-    void testPush() {
-
-    }
-
-    void testPop() {
-
-    }
-
-    void testIsEmpty() {
-
-    }
-
-    void testSize() {
-
-    }
-
-    void testResize() {
-
-    }
-}
+//package com.ljkdream.study
+//
+///**
+// * 测试代码
+// * Created by ljk on 16-1-22.
+// */
+//class FixedCapacityStackTest extends GroovyTestCase {
+//    void testPush() {
+//
+//    }
+//
+//    void testPop() {
+//
+//    }
+//
+//    void testIsEmpty() {
+//
+//    }
+//
+//    void testSize() {
+//
+//    }
+//
+//    void testResize() {
+//
+//    }
+//}
